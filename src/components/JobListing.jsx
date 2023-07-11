ce5c9870e5aedfffaa79d8b1ed7608108fc43e9f
yudistirarivaldi <yudistirarivaldii1234@gmail.com>
2023-07-11 22:27:01 +0800

    Pull Development

---
+++ src/components/JobListing.jsx
@@ -0,0 +1,314 @@
+import { Component } from "react";
+
+class JobListing extends Component {
+  render() {
+    return (
+      <>
+        <div className="py-10 px-10 m-auto p-auto container">
+          <div className="overflow-x-scroll pb-10 hide-scroll-bar">
+            <div className="flex flex-nowrap  ">
+              <div className="inline-block px-3">
+                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <div className="">
+                    <div className="group bg-gray-900 p-4">
+                      <div className=""></div>
+                      <div className="flex items-center gap-x-2">
+                        <img
+                          className="aspect-[2/2] w-16"
+                          src="https://idn-static-assets.s3-ap-southeast-1.amazonaws.com/school/10284.png"
+                        />
+                        <div>
+                          <h3 className="text-xl font-bold text-gray-50">
+                            Rakamin
+                          </h3>
+                          <span className="text-xs text-gray-300">
+                            Jakarta, Indonesia
+                          </span>
+                        </div>
+                      </div>
+                      <div className="my-4">
+                        <h3 className="text-2xl font-medium text-gray-200">
+                          Backend Developer
+                        </h3>
+                        <div className="text-sm font-medium">
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            PostgreSQL
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-yellow-500">
+                            Node JS
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-pink-500">
+                            Postman
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-lime-500">
+                            CI/CD
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            Docker
+                          </span>
+                        </div>
+                        <div className="mt-2 text-sm text-gray-400">
+                          $60K - $100K per year
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-50">
+                          Full Time
+                        </span>
+                        <button
+                          type="submit"
+                          className="font-medium text-blue-500 transition-all duration-300 group-hover:text-blue-500/80"
+                        >
+                          Apply Now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="inline-block px-3">
+                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <div className="">
+                    <div className="group bg-gray-900 p-4">
+                      <div className=""></div>
+                      <div className="flex items-center gap-x-2">
+                        <img
+                          className="aspect-[2/2] w-16"
+                          src="https://idn-static-assets.s3-ap-southeast-1.amazonaws.com/school/10284.png"
+                        />
+                        <div>
+                          <h3 className="text-xl font-bold text-gray-50">
+                            Rakamin
+                          </h3>
+                          <span className="text-xs text-gray-300">
+                            Jakarta, Indonesia
+                          </span>
+                        </div>
+                      </div>
+                      <div className="my-4">
+                        <h3 className="text-2xl font-medium text-gray-200">
+                          Backend Developer
+                        </h3>
+                        <div className="text-sm font-medium">
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            PostgreSQL
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-yellow-500">
+                            Node JS
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-pink-500">
+                            Postman
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-lime-500">
+                            CI/CD
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            Docker
+                          </span>
+                        </div>
+                        <div className="mt-2 text-sm text-gray-400">
+                          $60K - $100K per year
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-50">
+                          Intern
+                        </span>
+                        <button
+                          type="submit"
+                          className="font-medium text-blue-500 transition-all duration-300 group-hover:text-blue-500/80"
+                        >
+                          Apply Now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="inline-block px-3">
+                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <div className="">
+                    <div className="group bg-gray-900 p-4">
+                      <div className=""></div>
+                      <div className="flex items-center gap-x-2">
+                        <img
+                          className="aspect-[2/2] w-16"
+                          src="https://idn-static-assets.s3-ap-southeast-1.amazonaws.com/school/10284.png"
+                        />
+                        <div>
+                          <h3 className="text-xl font-bold text-gray-50">
+                            Rakamin
+                          </h3>
+                          <span className="text-xs text-gray-300">
+                            Jakarta, Indonesia
+                          </span>
+                        </div>
+                      </div>
+                      <div className="my-4">
+                        <h3 className="text-2xl font-medium text-gray-200">
+                          Backend Developer
+                        </h3>
+                        <div className="text-sm font-medium">
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            PostgreSQL
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-yellow-500">
+                            Node JS
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-pink-500">
+                            Postman
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-lime-500">
+                            CI/CD
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            Docker
+                          </span>
+                        </div>
+                        <div className="mt-2 text-sm text-gray-400">
+                          $60K - $100K per year
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-50">
+                          Full Time
+                        </span>
+                        <button
+                          type="submit"
+                          className="font-medium text-blue-500 transition-all duration-300 group-hover:text-blue-500/80"
+                        >
+                          Apply Now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="inline-block px-3">
+                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <div className="">
+                    <div className="group bg-gray-900 p-4">
+                      <div className=""></div>
+                      <div className="flex items-center gap-x-2">
+                        <img
+                          className="aspect-[2/2] w-16"
+                          src="https://idn-static-assets.s3-ap-southeast-1.amazonaws.com/school/10284.png"
+                        />
+                        <div>
+                          <h3 className="text-xl font-bold text-gray-50">
+                            Rakamin
+                          </h3>
+                          <span className="text-xs text-gray-300">
+                            Jakarta, Indonesia
+                          </span>
+                        </div>
+                      </div>
+                      <div className="my-4">
+                        <h3 className="text-2xl font-medium text-gray-200">
+                          Backend Developer
+                        </h3>
+                        <div className="text-sm font-medium">
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            PostgreSQL
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-yellow-500">
+                            Node JS
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-pink-500">
+                            Postman
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-lime-500">
+                            CI/CD
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            Docker
+                          </span>
+                        </div>
+                        <div className="mt-2 text-sm text-gray-400">
+                          $60K - $100K per year
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-50">
+                          Full Time
+                        </span>
+                        <button
+                          type="submit"
+                          className="font-medium text-blue-500 transition-all duration-300 group-hover:text-blue-500/80"
+                        >
+                          Apply Now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="inline-block px-3">
+                <div className="w-64 h-64 max-w-xs overflow-hidden rounded-lg shadow-md bg-white hover:shadow-xl transition-shadow duration-300 ease-in-out">
+                  <div className="">
+                    <div className="group bg-gray-900 p-4">
+                      <div className=""></div>
+                      <div className="flex items-center gap-x-2">
+                        <img
+                          className="aspect-[2/2] w-16"
+                          src="https://idn-static-assets.s3-ap-southeast-1.amazonaws.com/school/10284.png"
+                        />
+                        <div>
+                          <h3 className="text-xl font-bold text-gray-50">
+                            Rakamin
+                          </h3>
+                          <span className="text-xs text-gray-300">
+                            Jakarta, Indonesia
+                          </span>
+                        </div>
+                      </div>
+                      <div className="my-4">
+                        <h3 className="text-2xl font-medium text-gray-200">
+                          Backend Developer
+                        </h3>
+                        <div className="text-sm font-medium">
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            PostgreSQL
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-yellow-500">
+                            Node JS
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-pink-500">
+                            Postman
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-lime-500">
+                            CI/CD
+                          </span>
+                          <span className="m-1 ml-0 inline-block text-blue-500">
+                            Docker
+                          </span>
+                        </div>
+                        <div className="mt-2 text-sm text-gray-400">
+                          $60K - $100K per year
+                        </div>
+                      </div>
+                      <div className="flex items-center justify-between">
+                        <span className="text-sm font-medium text-gray-50">
+                          Full Time
+                        </span>
+                        <button
+                          type="submit"
+                          className="font-medium text-blue-500 transition-all duration-300 group-hover:text-blue-500/80"
+                        >
+                          Apply Now
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </>
+    );
+  }
+}
+
+export default JobListing;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
 import Footer from "../components/Footer";
+import JobListing from "../components/JobListing";
 
 export default function Home() {
     return <div>
 
+    <JobListing/>
     <Footer/>
 
     </div>;
