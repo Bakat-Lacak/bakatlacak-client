07a2aca94891d3e96ee73f4bce40ae82a2c3e006
Ilmanzi <ilman.aziss@gmail.com>
2023-07-11 21:14:32 +0700

    updating: progress

--- src/pages/UserProfile.jsx
+++ src/pages/UserProfile.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getUserProfileById, getUser } from "../fetching/userProfile";
 import { useStore } from "../modules/store";
+import image from "../assets/example.jpg"
 
 export default function UserProfilePage() {
   const [profile, setProfile] = useState({})
@@ -29,18 +30,23 @@ export default function UserProfilePage() {
 
   return (
     <>
-      <div className="flex">
-        <div>
-          <ul>
-            <li>Basic Info</li>
-            <li>Education</li>
-            <li>Experience</li>
+      <div className="flex gap-2">
+        <div className="p-4 flex-2 bg-mint">
+          <ul className="border uppercase">
+            <a className="button"><li className="p-1">Basic Info</li></a>
+            <li className="p-1 border-t border-b">Education</li>
+            <li className="p-1">Experience</li>
           </ul>
         </div>
-        <div>
-          <p>Email</p>
+        <div className="p-3 flex-1 bg-navy">
+        <img src={image} alt="Userphoto" width="100" height="250"></img>
+        <p>Nama : {user.first_name} {user.last_name}</p>
+
+
+
         </div>
-        {/* <p>User Profiles Page</p>
+        
+        {/* <p>User Profiles Page</p> 
         {JSON.stringify(profile)}
         {JSON.stringify(user)} */}
       </div>
