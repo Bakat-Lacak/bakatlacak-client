31b8c2fec5c10f63f85ff8e030690afb2549d00e
danielzuhad <danielzuhad@gmail.com>
2023-07-12 13:11:53 +0700

    add homepage but not finish

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,286 @@
 export default function Home() {
-    return <div>Homepage</div>;
+  return (
+    <section className="bg-[#DAFFFB] ">
+      {/* Heading */}
+      <div className="max-w-screen px-4 py-8 sm:px-6 sm:py-12 lg:px-8 lg:py-16 w-full h-screen flex justify-center items-center flex-row">
+        <div className="grid grid-cols-1 gap-y-10 lg:grid-cols-2 lg:items-center lg:gap-x-16 min-h-[500px]">
+          <div className="mx-auto max-w-2lg text-center lg:mx-0 ltr:lg:text-left rtl:lg:text-right">
+            <h2 className="text-3xl font-bold sm:text-7xl text-[#001C30]">
+              Find your career path
+            </h2>
+
+            <p className="mt-4 text-gray-600">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut vero
+              aliquid sint distinctio iure ipsum cupiditate? Quis, odit
+              assumenda? Deleniti quasi inventore, libero reiciendis minima
+              aliquid tempora. Obcaecati, autem.
+            </p>
+
+            <a
+              href="#"
+              className="mt-8 inline-block rounded bg-[#001C30] px-12 py-3 text-sm font-medium text-white transition hover:bg-indigo-700 focus:outline-none focus:ring focus:ring-yellow-400"
+            >
+              Get Started Today
+            </a>
+          </div>
+
+          <div className="grid grid-cols-2 gap-10 sm:grid-cols-3">
+            <a
+              className="block rounded-xl border border-[#176B87] p-4 shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white"
+              href="/accountant"
+            >
+              <span className="inline-block rounded-lg bg-[#001C30] text-white p-3">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <h2 className="mt-2 font-bold">Accountant</h2>
+
+              <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </a>
+            <a
+              className="block rounded-xl border border-[#176B87] p-4 shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white"
+              href="/accountant"
+            >
+              <span className="inline-block rounded-lg bg-[#001C30] text-white p-3">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <h2 className="mt-2 font-bold">Accountant</h2>
+
+              <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </a>
+            <a
+              className="block rounded-xl border border-[#176B87] p-4 shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white"
+              href="/accountant"
+            >
+              <span className="inline-block rounded-lg bg-[#001C30] text-white p-3">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <h2 className="mt-2 font-bold">Accountant</h2>
+
+              <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </a>
+            <a
+              className="block rounded-xl border border-[#176B87] p-4 shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white"
+              href="/accountant"
+            >
+              <span className="inline-block rounded-lg bg-[#001C30] text-white p-3">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <h2 className="mt-2 font-bold">Accountant</h2>
+
+              <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </a>
+            <a
+              className="block rounded-xl border border-[#176B87] p-4 shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white"
+              href="/accountant"
+            >
+              <span className="inline-block rounded-lg bg-[#001C30] text-white p-3">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <h2 className="mt-2 font-bold">Accountant</h2>
+
+              <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </a>
+            <a
+              className="block rounded-xl border border-[#176B87] p-4 shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white"
+              href="/accountant"
+            >
+              <span className="inline-block rounded-lg bg-[#001C30] text-white p-3">
+                <svg
+                  className="h-6 w-6"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path d="M12 14l9-5-9-5-9 5 9 5z"></path>
+                  <path d="M12 14l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14z"></path>
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 14l9-5-9-5-9 5 9 5zm0 0l6.16-3.422a12.083 12.083 0 01.665 6.479A11.952 11.952 0 0012 20.055a11.952 11.952 0 00-6.824-2.998 12.078 12.078 0 01.665-6.479L12 14zm-4 6v-7.5l4-2.222"
+                  ></path>
+                </svg>
+              </span>
+
+              <h2 className="mt-2 font-bold">Accountant</h2>
+
+              <p className="hidden sm:mt-1 sm:block sm:text-sm sm:text-gray-600">
+                Lorem ipsum dolor sit amet consectetur.
+              </p>
+            </a>
+          </div>
+        </div>
+      </div>
+      {/* Heading End */}
+
+      {/* Flow */}
+      <div className="min-h-screen w-full flex flex-col justify-center items-center">
+        <h1 className="text-3xl font-bold sm:text-4xl text-[#001C30] mb-[50px]">
+          How exactly you apply for the job
+        </h1>
+        <div className="flex gap-10 flex-col lg:flex-row">
+          <div className="flex flex-col p-5 justify-center items-center rounded-md border border-[#176B87]  shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white ">
+            <svg
+              className="h-[300px] w-[300px]"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 32 32"
+              id="resume"
+            >
+              <path d="M4.5 32h23a.5.5 0 0 0 .5-.5v-27a.5.5 0 0 0-.5-.5H21v-.5a.5.5 0 0 0-.5-.5H18c0-1.103-.897-2-2-2s-2 .897-2 2h-2.5a.5.5 0 0 0-.5.5V4H4.5a.5.5 0 0 0-.5.5v27a.5.5 0 0 0 .5.5zM14.592 4a.501.501 0 0 0 .471-.667A1.004 1.004 0 0 1 15 3c0-.551.449-1 1-1s1 .449 1 1c0 .104-.021.213-.063.333a.501.501 0 0 0 .471.667H20v2h-8V4h2.592zM5 5h6v1.5a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5V5h6v26H5V5z"></path>
+              <path d="M25.5 6h-3a.5.5 0 0 0 0 1H25v22H7V7h2.5a.5.5 0 0 0 0-1h-3a.5.5 0 0 0-.5.5v23a.5.5 0 0 0 .5.5h19a.5.5 0 0 0 .5-.5v-23a.5.5 0 0 0-.5-.5z"></path>
+              <path d="m9.544 10.153.286 1.046c.011.042.048.066.069.102v.555c0 .586.245 1.115.636 1.495v.519l-1.392.522a1.779 1.779 0 0 0-1.148 1.658v1.45a.5.5 0 0 0 1 0v-1.45c0-.319.201-.609.5-.721l.534-.2a2.71 2.71 0 0 0 1.959.858c.752 0 1.452-.325 1.962-.863l.545.204c.299.112.5.402.5.721v1.45a.5.5 0 0 0 1 0v-1.45c0-.733-.461-1.4-1.148-1.658l-1.407-.528v-.513c.39-.379.634-.908.634-1.494v-.52a.508.508 0 0 0 .068-.164l.297-1.379c.062-.336-.013-.676-.21-.956s-.491-.465-.829-.521l-1.81-.302a1.249 1.249 0 0 0-1.394.843 1.13 1.13 0 0 0-.652 1.296zm3.53 1.703c0 .397-.224.73-.543.92-.058.034-.114.07-.176.093a1.113 1.113 0 0 1-.272.055 1.058 1.058 0 0 1-.195 0 1.088 1.088 0 0 1-.27-.055c-.061-.022-.117-.058-.174-.092a1.073 1.073 0 0 1-.545-.921v-.289h2.175v.289zm-1.955 2.864.092-.035a.5.5 0 0 0 .324-.468v-.323l.01.001c.143.031.29.049.442.049h.001l.014-.001c.147-.001.29-.018.429-.048l.01-.001v.324c0 .219.142.399.338.467l.002.001.083.031a1.867 1.867 0 0 1-1.745.003zm-.579-4.905a.115.115 0 0 1 .094-.045c.276 0 .5-.244.5-.52 0-.102.056-.164.089-.192a.243.243 0 0 1 .205-.057l1.81.302c.096.016.15.076.174.11a.26.26 0 0 1 .048.186l-.209.968h-2.556l-.179-.651a.116.116 0 0 1 .024-.101zM17.5 12h6a.5.5 0 0 0 .5-.5v-2a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5zm.5-2h5v1h-5v-1zm6 9.5a.5.5 0 0 0-.5-.5h-15a.5.5 0 0 0-.5.5v2a.5.5 0 0 0 .5.5h15a.5.5 0 0 0 .5-.5v-2zM23 21H9v-1h14v1zm.5-8h-3a.5.5 0 0 0 0 1h3a.5.5 0 0 0 0-1zm-6 1h1a.5.5 0 0 0 0-1h-1a.5.5 0 0 0 0 1zm3 9h-10a.5.5 0 0 0 0 1h10a.5.5 0 0 0 0-1zm-11.65.149a.503.503 0 0 0-.7 0A.473.473 0 0 0 8 23.5c0 .069.01.13.04.189.02.061.06.12.11.16.04.05.1.09.16.11.06.031.12.041.19.041s.13-.01.19-.04a.372.372 0 0 0 .16-.11c.05-.04.09-.1.11-.16.03-.06.04-.121.04-.19 0-.13-.05-.26-.15-.351zM20.5 25h-10a.5.5 0 0 0 0 1h10a.5.5 0 0 0 0-1zm-11.65.149a.503.503 0 0 0-.7 0A.473.473 0 0 0 8 25.5c0 .14.05.26.15.35.09.1.22.15.35.15.07 0 .13-.01.19-.04a.372.372 0 0 0 .16-.11c.05-.04.09-.1.11-.16.03-.06.04-.121.04-.19 0-.13-.05-.26-.15-.351zM20.5 27h-10a.5.5 0 0 0 0 1h10a.5.5 0 0 0 0-1zm3-4h-1a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2h-1a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm0 2h-1a.5.5 0 0 0 0 1h1a.5.5 0 0 0 0-1zm-15.19.04c-.06.02-.12.06-.16.109-.1.091-.15.221-.15.351s.05.26.15.35c.09.1.22.15.35.15.13 0 .26-.05.35-.15.1-.09.15-.22.15-.35s-.05-.26-.15-.351a.479.479 0 0 0-.54-.109zM17.5 16h6a.5.5 0 0 0 0-1h-6a.5.5 0 0 0 0 1zm0 2h4a.5.5 0 0 0 0-1h-4a.5.5 0 0 0 0 1z"></path>
+            </svg>
+            <h2 className="font-bold py-2 text-2xl ">1. Drop Your CV</h2>
+          </div>
+
+          <div className="flex flex-col p-5 justify-center items-center rounded-md border border-[#176B87]  shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-[300px] w-[300px]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M12 6v6h4.5m4.5 0a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
+            </svg>
+
+            <h2 className="font-bold py-2 text-2xl ">
+              2. Wait HR Analyize Your CV
+            </h2>
+          </div>
+
+          <div className="flex flex-col p-5 justify-center items-center rounded-md border border-[#176B87]  shadow-md hover:border-[#001C30] hover:ring-1 hover:ring-blue-200 focus:outline-none focus:ring hover:shadow-[#001c3094] bg-white ">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-[300px] w-[300px]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+              />
+            </svg>
+
+            {/* <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth="1.5"
+              stroke="currentColor"
+              className="h-[300px] w-[300px]"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M15.182 15.182a4.5 4.5 0 01-6.364 0M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
+              />
+            </svg> */}
+
+            <h2 className="font-bold py-2 text-2xl ">3. Get Rejected</h2>
+          </div>
+        </div>
+      </div>
+      {/* Flow End */}
+    </section>
+  );
 }
