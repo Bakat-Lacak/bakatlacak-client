c6ae87c70aba735921a69e13848db31f92cc30de
Ilman <ilman.aziss@gmail.com>
2023-08-01 13:53:32 +0700

    fixing type timing

--- src/components/TypeAnimation.jsx
+++ src/components/TypeAnimation.jsx
@@ -7,23 +7,23 @@ export default function TypeAnimate() {
                 sequence={[
                     // Same substring at the start will only be typed once, initially
                     'Backend Developer',
-                    1000,
+                    3000,
                     'Frontend Developer',
-                    1000,
+                    3000,
                     'Digital Marketing',
-                    1000,
+                    3000,
                     'Data Analyst',
-                    1000,
+                    3000,
                     'Data Science',
-                    1000,
+                    3000,
                     'Product Manager',
-                    1000,
+                    3000,
                     'UI / UX Designer',
-                    1000,
+                    3000,
                     'Social Media Expert',
-                    1000,
+                    3000,
                 ]}
-                speed={15}
+                speed={10}
                 style={{ fontSize: 60 }}
                 repeat={Infinity}
             />
